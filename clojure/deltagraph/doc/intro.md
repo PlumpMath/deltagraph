fd025aa7f5059d5741a8fac5404373b087b5fcf5
nick rothwell <nick@cassiel.com>
2013-06-19 16:06:39 +0100

    Clojure project.

---
+++ clojure/deltagraph/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to deltagraph
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clojure/deltagraph/test/deltagraph/core_test.clj
@@ -0,0 +1,7 @@
+(ns deltagraph.core-test
+  (:use clojure.test
+        deltagraph.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
